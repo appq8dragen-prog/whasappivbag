0d2c0d6d8787196e4f3675ecebd5d5f8342153d2
appq8dragen-prog <app.q8dragen@gmail.com>
2026-06-05 21:46:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,17 @@ app.use(bodyParser.json());
 
 // إعداد عميل الواتساب مع حفظ الجلسة وتخطي قيود سيرفرات اللينكس
 const client = new Client({
-    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu'
+        ],
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null
+    }
+});    authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
         args: [
